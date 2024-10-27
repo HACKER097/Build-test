4cc75f67cad14306905b8447244e5249c7c4437f
hacker097 <shantanubhargava2005@gmail.com>
2024-10-27 20:32:30 +0530

    added everything

--- A^pp 2/challenge/app.py
+++ A^pp 2/challenge/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Dockerized Flask! COMMIT ONE"
+    return "Hello, Dockerized Flask! COMMIT ONE LALALALLA"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

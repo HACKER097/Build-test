dfaa6bcc516ebf3a22d8b5702de69db62e38c8c6
hacker097 <shantanubhargava2005@gmail.com>
2024-10-27 19:56:58 +0530

    added stuff

--- App# !1/challenge/app.py
+++ App# !1/challenge/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "EDIT EDIT EDIT"
+    return "EDIT EDIT EDIT EDIT"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
